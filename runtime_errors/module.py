68b336f0b58da06e347c2d3d61679698907fd5ee
DELLEST27 <jaidell.kennedy@stu.vertexacademies.org>
2026-02-27 19:17:29 +0000

    abcdefg

--- runtime_errors/module.py
+++ runtime_errors/module.py
@@ -1,3 +1,12 @@
 def isdigit(string):
     digits = ["0","1","2","3","4","5","6","7","8","9"]
-    pass
+    count=0
+    index=0
+
+    while index < len(digits)-1:
+        letter = string[index]
+
+        index += 1
+        if letter in digits :
+            count+= 1
+            return count
--- runtime_errors/secret_number.py
+++ runtime_errors/secret_number.py
@@ -2,14 +2,14 @@ from module import isdigit
 from os import system
 system("clear")
 
-secret_number = 333
+secret_number = 777
 
 print("Autheticating user...")
 pwd = input("What's the secret number? ") # User Input
 
 print()         # Skip Line
 
-if pwd == 333:  # Check authentication
+if pwd == 777:  # Check authentication
     print("Success! Opening RESTRICTED APP...")
 else:
     print("That's the wrong number.")
